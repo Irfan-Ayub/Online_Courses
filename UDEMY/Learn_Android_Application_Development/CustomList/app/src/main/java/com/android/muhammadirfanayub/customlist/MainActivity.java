5944ae4e0a1bef0fa2f81af34e7662ebf55fda6e
Muhammad Irfan Ayub <irfan.ayub786@outlook.com>
2018-08-01 09:31:06 +0500

    onclick change for taost btn

--- UDEMY/Learn_Android_Application_Development/CustomList/app/src/main/java/com/android/muhammadirfanayub/customlist/MainActivity.java
+++ UDEMY/Learn_Android_Application_Development/CustomList/app/src/main/java/com/android/muhammadirfanayub/customlist/MainActivity.java
@@ -30,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this , "Toast is Showing!" , Toast.LENGTH_SHORT).show();
 
                 showToast("My Custom Toast");
+
             }
         });
 
